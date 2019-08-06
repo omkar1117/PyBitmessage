f6c0ac96f578052de236d83c5fa8b3a96a0e0f79
omkar1117 <git.omme@gmail.com>
2019-08-06 22:28:03 +0530

    print statements compatible to python3

--- dev/ssltest.py
+++ dev/ssltest.py
@@ -9,7 +9,7 @@ HOST = "127.0.0.1"
 PORT = 8912
 
 
-def sslProtocolVersion():
+def sslprotocolversion():
     # sslProtocolVersion
     if sys.version_info >= (2, 7, 13):
         # this means TLSv1 or higher
@@ -25,7 +25,7 @@ def sslProtocolVersion():
         return ssl.PROTOCOL_TLSv1
 
 
-def sslProtocolCiphers():
+def sslprotocolciphers():
     if ssl.OPENSSL_VERSION_NUMBER >= 0x10100000:
         return "AECDH-AES256-SHA@SECLEVEL=0"
     else:
@@ -45,59 +45,60 @@ def listen():
     return sock
 
 
-def sslHandshake(sock, server=False):
+def sslhandshake(sock, server=False):
     if sys.version_info >= (2, 7, 9):
-        context = ssl.SSLContext(sslProtocolVersion())
-        context.set_ciphers(sslProtocolCiphers())
+        context = ssl.SSLContext(sslprotocolversion())
+        context.set_ciphers(sslprotocolciphers())
         context.set_ecdh_curve("secp256k1")
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
-        context.options = ssl.OP_ALL | ssl.OP_NO_SSLv2 | ssl.OP_NO_SSLv3 | ssl.OP_SINGLE_ECDH_USE | ssl.OP_CIPHER_SERVER_PREFERENCE
-        sslSock = context.wrap_socket(sock, server_side=server, do_handshake_on_connect=False)
+        context.options = ssl.OP_ALL | ssl.OP_NO_SSLv2 | ssl.OP_NO_SSLv3 | \
+                              ssl.OP_SINGLE_ECDH_USE | ssl.OP_CIPHER_SERVER_PREFERENCE
+        sslsock = context.wrap_socket(sock, server_side=server, do_handshake_on_connect=False)
     else:
-        sslSock = ssl.wrap_socket(sock, keyfile=os.path.join('src', 'sslkeys', 'key.pem'),
+        sslsock = ssl.wrap_socket(sock, keyfile=os.path.join('src', 'sslkeys', 'key.pem'),
                                   certfile=os.path.join('src', 'sslkeys', 'cert.pem'),
-                                  server_side=server, ssl_version=sslProtocolVersion(),
+                                  server_side=server, ssl_version=sslprotocolversion(),
                                   do_handshake_on_connect=False, ciphers='AECDH-AES256-SHA')
 
     while True:
         try:
-            sslSock.do_handshake()
+            sslsock.do_handshake()
             break
         except ssl.SSLWantReadError:
-            print "Waiting for SSL socket handhake read"
-            select.select([sslSock], [], [], 10)
+            print("Waiting for SSL socket handhake read")
+            select.select([sslsock], [], [], 10)
         except ssl.SSLWantWriteError:
-            print "Waiting for SSL socket handhake write"
-            select.select([], [sslSock], [], 10)
+            print("Waiting for SSL socket handhake write")
+            select.select([], [sslsock], [], 10)
         except Exception:
-            print "SSL socket handhake failed, shutting down connection"
+            print("SSL socket handhake failed, shutting down connection")
             traceback.print_exc()
             return
-    print "Success!"
-    return sslSock
+    print("Success!")
+    return sslsock
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print "Usage: ssltest.py client|server"
+        print("Usage: ssltest.py client|server")
         sys.exit(0)
     elif sys.argv[1] == "server":
         serversock = listen()
         while True:
-            print "Waiting for connection"
+            print("Waiting for connection")
             sock, addr = serversock.accept()
-            print "Got connection from %s:%i" % (addr[0], addr[1])
-            sslSock = sslHandshake(sock, True)
+            print("Got connection from %s:%i" % (addr[0], addr[1]))
+            sslSock = sslhandshake(sock, True)
             if sslSock:
                 sslSock.shutdown(socket.SHUT_RDWR)
                 sslSock.close()
     elif sys.argv[1] == "client":
         sock = connect()
-        sslSock = sslHandshake(sock, False)
+        sslSock = sslhandshake(sock, False)
         if sslSock:
             sslSock.shutdown(socket.SHUT_RDWR)
             sslSock.close()
     else:
-        print "Usage: ssltest.py client|server"
+        print("Usage: ssltest.py client|server")
         sys.exit(0)
